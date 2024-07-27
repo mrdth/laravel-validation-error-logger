60f1e4f4625e488af2843f95a4b68c33caec0e80
mrdth <mrdth@users.noreply.github.com>
2024-07-27 22:32:29 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Mrdth\LaravelValidationErrorLogger\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Mrdth\LaravelValidationErrorLogger\LaravelValidationErrorLoggerServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
